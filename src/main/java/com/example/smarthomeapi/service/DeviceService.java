4ade576c506b5ef2ac1dffcca31cacce5d3a5ee0
M. Numan KARACA <sekercaytuz@gmail.com>
2025-10-04 20:24:07 +0300

    some changes

--- src/main/java/com/example/smarthomeapi/service/DeviceService.java
+++ src/main/java/com/example/smarthomeapi/service/DeviceService.java
@@ -32,42 +32,34 @@ public class DeviceService {
     }
 
     public Device addDevice(Device newDevice) {
-        // Yeni cihaza benzersiz bir ID ata
         Long newId = counter.incrementAndGet();
         newDevice.setId(newId);
 
-        // Cihazı listeye ekle
         deviceList.add(newDevice);
 
-        // Eklenen cihazı geri döndür
         return newDevice;
     }
 
     public Optional<Device> updateDevice(Long id, Device deviceDetails) {
-        // Önce güncellenecek cihazı ID'sine göre bulalım.
+
         Optional<Device> deviceOptional = getDeviceById(id);
 
-        // Eğer cihaz bulunduysa...
         if (deviceOptional.isPresent()) {
             Device existingDevice = deviceOptional.get();
 
-            // Mevcut cihazın bilgilerini yeni gelenlerle güncelle.
             existingDevice.setName(deviceDetails.getName());
             existingDevice.setStatus(deviceDetails.isStatus());
 
-            // Güncellenmiş cihazı Optional içinde geri döndür.
+
             return Optional.of(existingDevice);
         } else {
-            // Cihaz bulunamadıysa, boş bir Optional döndür.
+
             return Optional.empty();
         }
     }
 
 
     public boolean deleteDevice(Long id) {
-        // List.removeIf metodu, verilen koşulu sağlayan elemanı listeden siler.
-        // Eğer bir eleman silindiyse 'true', aksi halde 'false' döndürür.
-        // Bu, aradığımız ID'de bir cihaz olup olmadığını ve silinip silinmediğini tek satırda kontrol etmemizi sağlar.
         return deviceList.removeIf(device -> device.getId().equals(id));
     }
 
