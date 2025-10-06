724a824285ccfe10d0e7137f9ec11b029316a606
M. Numan KARACA <sekercaytuz@gmail.com>
2025-10-06 17:16:45 +0300

    Some changes

--- src/main/java/com/example/smarthomeapi/service/DeviceService.java
+++ src/main/java/com/example/smarthomeapi/service/DeviceService.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicLong;
-
+import java.util.concurrent.atomic.AtomicLong;
 
 @Service
 public class DeviceService {
